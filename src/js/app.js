dcc651abd5b921c64d66bce3512cd2467a65763b
古市直哉 <furuichi@nuxil.jp>
2023-06-06 20:23:05 +0900

    push

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,10 @@ import {
   gsap,
 } from 'gsap';
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(DrawSVGPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.set("#path01,#path02,#path03", {
   drawSVG : '0%',
@@ -18,4 +20,20 @@ gsap.to("#path01,#path02,#path03", {
   drawSVG : '100%',
   duration: 1.4,
   ease:"power1.inOut"
+});
+
+
+gsap.set(".scroll-wrap", {
+  x: 100
+});
+gsap.to(".scroll-wrap", {
+  x: -100,
+  scrollTrigger: {
+    trigger: '.asset__scroll-text',
+    start: 'top bottom',
+    end: 'bottom top',
+    scrub: true,
+    invalidateOnRefresh: true,
+    markers: true,
+  }
 });
